9e37f3ee75f967781a22418fd8904a88cc2817e7
19alema <75095003+19alema@users.noreply.github.com>
2022-03-26 15:36:06 +0300

    upadted commit

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -7,15 +7,15 @@ import './TextContainer.css';
 const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
-      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
+      <h1>EddieBook <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Chat, Rest and Enjoy<span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>Lets try it out now<span role="img" aria-label="emoji">⬅️</span></h2>
     </div>
     {
       users
         ? (
           <div>
-            <h1>People currently chatting:</h1>
+            <h1>People on Chat:</h1>
             <div className="activeContainer">
               <h2>
                 {users.map(({name}) => (
